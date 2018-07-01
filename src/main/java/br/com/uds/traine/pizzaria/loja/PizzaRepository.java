0f7dcc4b5381edf566595c31685e209d8ac76e45
Alison Tadeu Veloza <alison.veloza@gmail.com>
2018-07-01 17:05:02 -0300

    exerciocio3

---
+++ src/main/java/br/com/uds/traine/pizzaria/loja/PizzaRepository.java
@@ -0,0 +1,14 @@
+package br.com.uds.traine.pizzaria.loja;
+
+import java.util.Set;
+import java.util.UUID;
+
+public interface PizzaRepository {
+    Pizza save(Pizza pizza);
+
+    Pizza findOne(UUID id);
+
+    Set<Pizza> findAll();
+
+    void delete(UUID id);
+}
